4418c5ff51d79bcd22f1af29f17799a2a10337bb
Alexis-Diaz <alex.d4556@gmail.com>
2022-09-29 19:19:10 -0600

    Introducción de la guía de estudio

---
+++ 01-Introduccion/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01_Introduccion
+{
+    //Esta guía se ha realizado en NET 5 y c# 9
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
